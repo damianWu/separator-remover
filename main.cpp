1cfcaa305e7275b246c9bb4deb883b7729ba1873
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2021-11-26 12:49:21 +0100

    Added two examples

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@
 #include "punct_stream/punct_stream.hpp"
 #include "utils/exception.hpp"
 
+// Example #1
+//
+// Init:    case senstitive -> true
+// Input:   For any integer N, do sth.
+// Output:  For any integer N do sth
+
+// Example #2
+//
+// Init:    case senstitive -> false
+// Input:   for any; integer N, do sth.
+// Output:  for any integer n do sth
+
 int main() {
     try {
         PunctStream ps{std::cin};
